ebc99bd4fd1721f49941843ca651262cd1ac0dd4
Emiliano Sangoi <esangoi@santafe.gov.ar>
2020-11-13 12:27:14 -0300

    Nueva fuenta y cambios en el index.

--- index.php
+++ index.php
@@ -34,192 +34,81 @@ $post = null;
 <?php
 // liberar memoria
 //unset($posts, $post);
+
 ?>
 
-<div id="inicio">
+<div>
 
-<<<<<<< HEAD
     <!-- Page Preloder -->
     <div id="preloder">
         <div class="loader"></div>
     </div>
 
-    <!-- Offcanvas Menu Begin -->
-    <div class="offcanvas-menu-overlay"></div>
-    <div class="offcanvas-menu ">
-        <div class="offcanvas__logo">
-            <a href="./index.html"><img src="<?php echo DIR_IMGS . '/logo_derecha.png' ?>" alt=""></a>
-        </div>
-        <div id="mobile-menu-wrap"></div>
-        <div class="offcanvas__widget">
-=======
-<!-- Page Preloder -->
-<div id="preloder">
-    <div class="loader"></div>
-</div>
 
->>>>>>> 74238bf918aa3f8f8fb575eb3c31b245eae50c94
+    <?php
+        get_template_part('partials/carousel');
+    ?>
 
 
-        </div>
-        <nav class="header__menu"> 
-            <ul class="mobile-menu">  
-                <li class="CloseMobileMenu"><a onclick="CloseMobileMenu()">X</a></li>
-                <li><a href="index.html"><i class="fa fa-home" aria-hidden="true"></i>
-                    </a></li>
-                <li><a href="#">Institucional</a>
-                    <ul class="dropdown">
-                        <li>
-                            <a href="<?php echo esc_url(get_page_link(PAGINA_QUIENES_SOMOS)); ?>">
-                                Quiénes somos
-                            </a>
-                        </li>
-                        <li><a href="historia.html">Un poco de historia</a></li>
+    <div class="container">
 
-                        <li><a href="politicas.html">Política de calidad</a></li>
-                        <li><a href="delegaciones.html">Delegaciones</a></li>
-
-                    </ul>
-                </li>
-                <li><a href="prestaciones.html">Prestaciones</a></li>
-
-                <!--get_permalink( get_page_by_path( 'contact' ) );-->
-                <li>
-                    <a href="<?php echo esc_url(get_page_link(PAGINA_ORG_APORTANTES)); ?>">
-                        Organismos aportantes
-                    </a>
-                </li>
-
-                <li><a href="servicios.html">Servicios</a></li>
-                <li><a href="contacto.html" >Contacto</a></li>
-            </ul>
-        </nav>
-        <div class="offcanvas__social">
-            <a href="#"><i class="fa fa-facebook"></i></a>
-            <a href="#"><i class="fa fa-twitter"></i></a>
-            <a href="#"><i class="fa fa-instagram"></i></a>
-            <a href="#"><i class="fa fa-linkedin"></i></a>
-        </div>
-    </div>
-    <!-- Offcanvas Menu End -->
-
-
-    <!-- Header Section Begin -->
-    <?php get_template_part("partials/main-nav"); ?>
-    <!-- Header Section End -->
-
-    <!-- Hero Section Begin -->
-    <section class="">
-        <div class="hero__sliders owl-carousel">
-            <div class="pruebaMenu set-bg" data-setbg="<?php echo DIR_IMGS . '/hero/hero-3.png' ?>">
-                <div class="container">  
-                    <div class="row">
-                        <div class="col-lg-9 centered">
-                            <div class="texto-img">
-
-                                <h2>La caja en números</h2><a href="lacajaennumeros.html">
-                                    <strong><h3>- VER -</h3></strong>
+        <!-- Team Section Begin -->
+        <section class="instructor spad">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-4 col-sm-6">
+                        <div class="instructor__item">
+                            <div class="instructor__item__pic"> 
+                                <a href="http://www.santafe.gov.ar/index.php/web/content/view/full/128047" target="_blank">
+                                    <img src="<?php echo DIR_IMGS . '/instructor/banners-02.png' ?>" alt="">
                                 </a>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="pruebaMenu set-bg" data-setbg="<?php echo DIR_IMGS . '/hero/hero-4.png' ?>">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-9 centered">
-                            <div class="texto-img">
-
-                                <h2>Medidas prevención Covid-19</h2><a href="https://www.santafe.gob.ar/ms/covid19/"><strong><h3>- VER -</h3></strong></a>
+                    <div class="col-lg-3 col-md-4 col-sm-6">
+                        <div class="instructor__item">
+                            <div class="instructor__item__pic"> 
+                                <a href="http://turnos.santafe.gov.ar/turnos/web/frontend.php" target="_blank">
+                                    <img src="<?php echo DIR_IMGS . '/instructor/banners-03.png' ?>" alt="">
+                                </a>
                             </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-sm-6">
+                        <div class="instructor__item">
+                            <div class="instructor__item__pic"> <a href="biblio_instructivos.html">
+                                    <img src="<?php echo DIR_IMGS . '/instructor/banners-04.png' ?>" alt=""></a></div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-sm-6">
+                        <div class="instructor__item">
+                            <div class="instructor__item__pic"> <a href="novedades.html">
+                                    <img src="<?php echo DIR_IMGS . '/instructor/banners-05-05.png' ?>" alt=""></a></div>
                         </div>
                     </div>
                 </div>
             </div>
-<<<<<<< HEAD
-            <div class="pruebaMenu set-bg" data-setbg="<?php echo DIR_IMGS . '/hero/hero-2.png' ?>">
-                <div class="container">  
-                    <div class="row">
-                        <div class="col-lg-9 centered">
-                            <div class="texto-img">		
-                                <h2> ¿Cómo tramito mis beneficios?</h2>
-                                <a href="prestaciones.html"><strong><h3>- VER -</h3></strong></a>
-                            </div>
-=======
-        </div>
-        <div class="pruebaMenu set-bg" data-setbg="<?php echo DIR_IMGS . '/hero/hero-2.png'  ?>">
-            <div class="container">  
+        </section>
+        <!-- Team Section End -->
+
+        <!-- Chooseus Section Begin -->
+        <section class="chooseus spad">
+            <div class="container">
                 <div class="row">
-                    <div class="col-lg-9 centered">
-                        <div class="texto-img">		
-                            <h2> ¿Cómo tramito mis beneficios?</h2>
-                            <a href="<?php echo esc_url(get_page_link(PAGINA_PRESTACIONES)); ?>"><strong><h3>- VER -</h3></strong></a>
->>>>>>> 74238bf918aa3f8f8fb575eb3c31b245eae50c94
-                        </div>
-                    </div>
+                    <span class="chooseus__pic"><a href="descarga_recibo.html">
+                            <img src="<?php echo DIR_IMGS . '/boton-07.png' ?>" alt=""></a>
+                    </span>
+                    <a href="biblioteca.html">
+                        <img src="<?php echo DIR_IMGS . '/boton-08.png' ?>" alt="">
+                    </a>                         
                 </div>
             </div>
-        </div>
-    </section>
-
-    <!-- Team Section Begin -->
-    <section class="instructor spad">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="instructor__item">
-                        <div class="instructor__item__pic"> 
-                            <a href="http://www.santafe.gov.ar/index.php/web/content/view/full/128047" target="_blank">
-                                <img src="<?php echo DIR_IMGS . '/instructor/banners-02.png' ?>" alt="">
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="instructor__item">
-                        <div class="instructor__item__pic"> 
-                            <a href="http://turnos.santafe.gov.ar/turnos/web/frontend.php" target="_blank">
-                                <img src="<?php echo DIR_IMGS . '/instructor/banners-03.png' ?>" alt="">
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="instructor__item">
-                        <div class="instructor__item__pic"> <a href="biblio_instructivos.html">
-                                <img src="<?php echo DIR_IMGS . '/instructor/banners-04.png' ?>" alt=""></a></div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="instructor__item">
-                        <div class="instructor__item__pic"> <a href="novedades.html">
-                                <img src="<?php echo DIR_IMGS . '/instructor/banners-05-05.png' ?>" alt=""></a></div>
-                    </div>
-                </div>
+            <div class="col-lg-6">
+                <div class="chooseus__pic"></div>
             </div>
-        </div>
-    </section>
-    <!-- Team Section End -->
+        </section>
+        <!-- Chooseus Section End -->
 
-    <!-- Chooseus Section Begin -->
-    <section class="chooseus spad">
-        <div class="container">
-            <div class="row">
-                <span class="chooseus__pic"><a href="descarga_recibo.html">
-                        <img src="<?php echo DIR_IMGS . '/boton-07.png' ?>" alt=""></a>
-                </span>
-                <a href="biblioteca.html">
-                    <img src="<?php echo DIR_IMGS . '/boton-08.png' ?>" alt="">
-                </a>                         
-            </div>
-        </div>
-        <div class="col-lg-6">
-            <div class="chooseus__pic"></div>
-        </div>
-    </section>
-    <!-- Chooseus Section End -->
+    </div>
 
-</div>
-
-<?php get_footer(); ?>
+    <?php get_footer(); ?>
---
+++ partials/carousel-hero.php
@@ -0,0 +1,46 @@
+<!-- Hero Section Begin -->
+<section class="">
+    <div class="hero__sliders owl-carousel">
+        <div class="pruebaMenu set-bg" data-setbg="<?php echo DIR_IMGS . '/hero/hero-3.png'  ?>">
+            <div class="container">  
+                <div class="row">
+                    <div class="col-lg-9 centered">
+                        <div class="texto-img">
+
+                            <h2>La caja en números</h2>
+                            <a href="lacajaennumeros.html">
+                                <strong>
+                                    <h3>- VER -</h3>
+                                </strong>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="pruebaMenu set-bg" data-setbg="<?php echo DIR_IMGS . '/hero/hero-4.png'  ?>">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-9 centered">
+                        <div class="texto-img">
+                            <h2>Medidas prevención Covid-19</h2>
+                            <a href="https://www.santafe.gob.ar/ms/covid19/"><strong><h3>- VER -</h3></strong></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="pruebaMenu set-bg" data-setbg="<?php echo DIR_IMGS . '/hero/hero-2.png'  ?>">
+            <div class="container">  
+                <div class="row">
+                    <div class="col-lg-9 centered">
+                        <div class="texto-img">		
+                            <h2> ¿Cómo tramito mis beneficios?</h2>
+                            <a href="<?php echo esc_url(get_page_link(PAGINA_PRESTACIONES)); ?>"><strong><h3>- VER -</h3></strong></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
