dda293268eee21e758a9a4d39da12c64ddf157ce
Eliana Mossimann <emossimann@santafe.gov.ar>
2020-11-17 07:18:16 -0300

    Se modifico el .gitignore y el sanitizar_archivos.php

--- util/sanitizar_archivos.php
+++ util/sanitizar_archivos.php
@@ -1,18 +1,20 @@
 <?php
 
 
-$dir = '/home';
-$dir_copia = '';
+$dir = '/home/eliana/vagrant/www/wp-caja/wp-content/themes/sitio_lacaja/biblio_normativas/';
+$dir_copia = '/home/eliana/vagrant/www/wp-caja/wp-content/themes/sitio_lacaja/biblio_normativas/NORMATIVAS/';
 $archivos = scandir($dir);
 
 // poner aca los archivos a omitir:
-$archivos_a_omitir = array(".","..");
+$archivos_a_omitir = array(".","..","NORMATIVAS");
 
 foreach($archivos as $archivo){
 	if (in_array($archivo, $archivos_a_omitir)){
 		continue;
 	}
 
-	$archivo = str_replace(array(' '), array('_'), $archivo);	
-	copy($dir . $archivo, $dir_copia . $archivo);
+	$archivo_nuevo = str_replace(array(' '), array('_'), $archivo);	
+        $archivo_nuevo = 'NORM_' . $archivo_nuevo;
+//        echo $archivo . "\n" ;
+	copy($dir . $archivo, $dir_copia . $archivo_nuevo);
 }
