09b7fcac7117719e3d7ffbe7fe32ea45db08181e
Lauriana Cattaneo <lcattaneo@santafe.gov.ar>
2020-11-20 12:00:04 -0300

    Se agrego contenido en sitios de interes

--- templates/sitios_de_interes.php
+++ templates/sitios_de_interes.php
@@ -18,7 +18,6 @@ get_header();
 </div>-->
 
 
-
 <div class="container-lg navbar-separator px-5 pt-4 pb-5 altura-minima" id="sitios-de-interes">
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
@@ -78,304 +77,106 @@ get_header();
                 <div class="p-lg-5 p-3">
                     <?php
                     $titulo = 'Gobierno de la Provincia de Santa Fe';
-                    $enlace = 'https://www.santafe.gov.ar';
+                    $link = 'https://www.santafe.gov.ar';
                     include locate_template('partials/enlace-interes.php');
 
                     $titulo = 'I.A.P.O.S. (Instituto Autárquico Provincial de Obra Social)';
-                    $enlace = 'http://www.iapossantafe.gob.ar';
+                    $link = 'http://www.iapossantafe.gob.ar';
                     include locate_template('partials/enlace-interes.php');
 
                     $titulo = 'Caja de Previsión Social de los Agentes Civiles del Estado (Seguro Mutual)';
-                    $enlace = 'http://www.cajadeprevisionsf.org.ar';
+                    $link = 'http://www.cajadeprevisionsf.org.ar';
                     include locate_template('partials/enlace-interes.php');
 
                     $titulo = 'Caja de Pensiones Sociales - Ley 5110';
-                    $enlace = 'https://www.santafe.gob.ar/index.php/web/content/view/full/234992';
+                    $link = 'https://www.santafe.gob.ar/index.php/web/content/view/full/234992';
                     include locate_template('partials/enlace-interes.php');
 
                     $titulo = 'ANSeS';
-                    $enlace = 'https://www.anses.gob.ar';
+                    $link = 'https://www.anses.gob.ar';
                     include locate_template('partials/enlace-interes.php');
 
                     $titulo = 'AFIP';
-                    $enlace = 'https://www.afip.gob.ar/sitio/externos/default.asp';
+                    $link = 'https://www.afip.gob.ar/sitio/externos/default.asp';
                     include locate_template('partials/enlace-interes.php');
 
                     $titulo = 'Consejo Federal de Previsión Social';
-                    $enlace = 'http://www.cofepres.org.ar';
+                    $link = 'http://www.cofepres.org.ar';
                     include locate_template('partials/enlace-interes.php');
-                    
-                ?>
+                    ?>
                 </div>
 
             </div>
             <div class="tab-pane" id="cajas-muni" role="tabpanel" aria-labelledby="tab-cajas-muni">
-                lololo
+                <div class="p-lg-5 p-3">
+                    <?php
+                    $titulo = 'Caja Municipalidad de Jubilaciones y Pensiones de Santa Fe';
+                    $link = 'http://www.cajamunicipal.gov.ar';
+                    include locate_template('partials/enlace-interes.php');
+
+                    $titulo = 'Instituto Municipal de Previsión Social Rosario)';
+                    $link = 'https://impsr.gob.ar';
+                    include locate_template('partials/enlace-interes.php');
+
+                    $titulo = 'Instituto Municipal de Previsión Social de Venado Tuerto';
+                    $link = 'https://www.impsvt.org.ar';
+                    include locate_template('partials/enlace-interes.php');
+
+                    $titulo = 'Caja Municipal de Previsión Social de Esperanza';
+                    $link = 'http://cajaesperanza.gob.ar';
+                    include locate_template('partials/enlace-interes.php');
+
+                    $titulo = 'Caja de Jubilaciones y Pensiones de la Municipalidad de Cañada de Gómez';
+                    $link = 'https://cañadadegomez.gob.ar';
+                    include locate_template('partials/enlace-interes.php');
+
+                    $titulo = 'Caja de Jubilados y Pensionados Municipales de Rufino';
+                    $link = 'http://rufino.gob.ar';
+                    include locate_template('partials/enlace-interes.php');
+                    ?>
+                </div>
             </div>
             <div class="tab-pane" id="cajas-prof" role="tabpanel" aria-labelledby="tab-cajas-prof">
-                lelele
+                <div class="p-lg-5 p-3">
+                    <?php
+                    $titulo = 'Caja de Seguridad Social para los Profesionales del Arte de Curar de la Provincia de Santa Fe';
+                    $link = 'https://www.cpac.org.ar/cpacv3/';
+                    include locate_template('partials/enlace-interes.php');
+
+                    $titulo = 'Caja de Previsión Social de los Profesionales de la Ingeniería de la Provincia de Santa Fe - 1° Circunscripción';
+                    $link = 'http://www.cajaingenieria.org';
+                    include locate_template('partials/enlace-interes.php');
+
+                    $titulo = 'Caja de Previsión Social de los Profesionales de la Ingeniería de la Provincia de Santa Fe - 2° Circunscripción';
+                    $link = 'http://www.cajaingros.com.ar';
+                    include locate_template('partials/enlace-interes.php');
+
+                    $titulo = 'Caja de Seguridad Social de Abogados y Procuradores de la Provincia de Santa Fe';
+                    $link = 'http://www.capsantafe.org.ar';
+                    include locate_template('partials/enlace-interes.php');
+
+                    $titulo = 'Caja de Seguridad Social para los Profesionales en Ciencias Económicas - Cámara I';
+                    $link = 'https://cpcesfe1.org.ar';
+                    include locate_template('partials/enlace-interes.php');
+
+                    $titulo = 'Caja de Seguridad Social para los Profesionales en Ciencias Económicas - Cámara II';
+                    $link = 'https://www.cpcesfe2.org.ar/Index.php';
+                    include locate_template('partials/enlace-interes.php');
+
+                    $titulo = 'Caja Notarial de Acción Social del Colegio de Escribanos de la Provincia de Santa Fe';
+                    $link = 'https://www.coessfe1.org.ar/portal/index.php/es/';
+                    include locate_template('partials/enlace-interes.php');
+
+                    $titulo = 'Caja Notarial de Acción Social del Colegio de Escribanos - 2° Circunscripción - Rosario';
+                    $link  = 'https://escribanos-stafe2da.org.ar';
+                    include locate_template('partials/enlace-interes.php');
+                    ?>
+                </div>
             </div>            
         </div>
 
     </div>
 
-
-    <!--============================================================================================================================= -->
-
-    <!--    <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="https://www.santafe.gov.ar" target="_blank">
-                    Acceder
-                </a>
-                Gobierno de la Provincia de Santa Fe
-            </div>
-    
-        </div>-->
-    <!--=========================================================================================-->
-
-    <!--    <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="http://www.iapossantafe.gob.ar" target="_blank">
-                    Acceder
-                </a>
-                I.A.P.O.S.
-            </div>
-    
-        </div>-->
-    <!--=========================================================================================-->
-
-    <!--    <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="http://www.cajadeprevisionsf.org.ar" target="_blank">
-                    Acceder
-                </a>
-                Caja de Previsión Social de los Agentes Civiles del Estado (Seguro Mutual)
-            </div>
-    
-        </div>-->
-    <!--=========================================================================================-->
-
-    <!--    <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="https://www.santafe.gob.ar/index.php/web/content/view/full/234992" target="_blank">
-                    Acceder
-                </a>
-                Caja de Pensiones Sociales - Ley 5110
-            </div>
-        </div>-->
-    <!--=========================================================================================-->
-
-    <!--    <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="https://www.anses.gob.ar" target="_blank">
-                    Acceder
-                </a>
-                ANSeS
-            </div>
-    
-        </div>-->
-    <!--=========================================================================================-->
-
-    <!--    <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="https://www.afip.gob.ar/sitio/externos/default.asp" target="_blank">
-                    Acceder
-                </a>
-                AFIP
-            </div>
-    
-        </div>               -->
-    <!--=========================================================================================--> 
-
-    <!--    <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="http://www.cofepres.org.ar" target="_blank">
-                    Acceder  
-                </a>
-                Consejo Federal de Previsión Social
-            </div>
-        </div>               -->
-    <!--=========================================================================================--> 
-
-    <!--</div>-->
-
-    <!--============================================================================================================================= -->
-
-    <div class="enlaces mt-4">        
-        <h4 class="h4 font-weight-bold">
-            CAJAS MUNICIPALES
-        </h4>
-
-        <!--=========================================================================================-->
-        <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="http://www.cajamunicipal.gov.ar" target="_blank">
-                    Acceder
-                </a>
-                Caja Municipalidad de Jubilaciones y Pensiones de Santa Fe
-            </div>
-
-        </div>
-        <!--=========================================================================================-->
-
-        <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="https://impsr.gob.ar" target="_blank">
-                    Acceder
-                </a>
-                Instituto Municipal de Previsión Social Rosario
-            </div>
-
-        </div>
-        <!--=========================================================================================-->
-
-        <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="https://www.impsvt.org.ar" target="_blank">
-                    Acceder
-                </a>
-                Instituto Municipal de Previsión Social de Venado Tuerto
-            </div>
-
-        </div>
-        <!--=========================================================================================-->
-
-        <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="http://cajaesperanza.gob.ar" target="_blank">
-                    Acceder
-                </a>
-                Caja Municipal de Previsión Social de Esperanza
-            </div>
-
-        </div>
-        <!--=========================================================================================-->
-
-        <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="https://cañadadegomez.gob.ar" target="_blank">
-                    Acceder
-                </a>
-                Caja de Jubilaciones y Pensiones de la Municipalidad de Cañada de Gómez
-            </div>
-
-        </div>
-        <!--=========================================================================================-->
-
-        <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="http://rufino.gob.ar" target="_blank">
-                    Acceder
-                </a>
-                Caja de Jubilados y Pensionados Municipales de Rufino
-            </div>
-
-        </div>               
-        <!--=========================================================================================--> 
-
-    </div>
-
-    <!--============================================================================================================================= -->
-    <div class="enlaces mt-4">        
-        <h4 class="h4 font-weight-bold">
-            CAJAS PROFESIONALES
-        </h4>
-
-        <!--=========================================================================================-->
-        <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="https://www.cpac.org.ar/cpacv3/" target="_blank">
-                    Acceder
-                </a>
-                Caja de Seguridad Social para los Profesionales del Arte de Curar de la Provincia de Santa Fe
-            </div>
-
-        </div>
-        <!--=========================================================================================-->
-
-        <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="http://www.cajaingenieria.org" target="_blank">
-                    Acceder
-                </a>
-                Caja de Previsión Social de los Profesionales de la Ingeniería de la Provincia de Santa Fe - 1° Circunscripción
-            </div>
-
-        </div>
-        <!--=========================================================================================-->
-
-        <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="http://www.cajaingros.com.ar" target="_blank">
-                    Acceder
-                </a>
-                Caja de Previsión Social de los Profesionales de la Ingeniería de la Provincia de Santa Fe - 2° Circunscripción
-            </div>
-
-        </div>
-        <!--=========================================================================================-->
-
-        <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="http://www.capsantafe.org.ar" target="_blank">
-                    Acceder
-                </a>
-                Caja de Seguridad Social de Abogados y Procuradores de la Provincia de Santa Fe
-            </div>
-
-        </div>
-        <!--=========================================================================================-->
-
-        <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="https://cpcesfe1.org.ar" target="_blank">
-                    Acceder
-                </a>
-                Caja de Seguridad Social para los Profesionales en Ciencias Económicas - Cámara I
-            </div>
-
-        </div>
-        <!--=========================================================================================-->
-
-        <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="https://www.cpcesfe2.org.ar/Index.php" target="_blank">
-                    Acceder
-                </a>
-                Caja de Seguridad Social para los Profesionales en Ciencias Económicas - Cámara II
-            </div>
-
-        </div>               
-        <!--=========================================================================================--> 
-
-        <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="https://www.coessfe1.org.ar/portal/index.php/es/" target="_blank">
-                    Acceder
-                </a>
-                Caja Notarial de Acción Social del Colegio de Escribanos de la Provincia de Santa Fe
-            </div>
-
-        </div>
-        <!--=========================================================================================-->
-
-        <div class="enlaces mt-4"> 
-            <div class="my-2 mb-3">
-                <a class="btn btn-outline-primary" href="https://escribanos-stafe2da.org.ar" target="_blank">
-                    Acceder
-                </a>
-                Caja Notarial de Acción Social del Colegio de Escribanos - 2° Circunscripción - Rosario
-            </div>
-
-        </div>
-        <!--=========================================================================================-->
-
-    </div>
-
 </div>
-
-
-<!-- Faq End -->
-<!-- About Section End -->
 
 <?php get_footer(); ?>
