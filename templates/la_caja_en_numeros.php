b40775ee762d143c4b58125fc9d33f3098d4c4d7
Emiliano Sangoi <esangoi@santafe.gov.ar>
2021-02-02 14:26:28 -0300

    Caja en numeros
    Se cambio el formato de las estadisticas a pdf embebidos.

--- templates/la_caja_en_numeros.php
+++ templates/la_caja_en_numeros.php
@@ -5,7 +5,7 @@
 // La ubicacion de este codigo debe ser al principio del archivo.
 $pagina = get_page_by_path(PAGINA_LA_CAJA_EN_NUMEROS);
 if (!$pagina instanceof WP_Post || $pagina->post_status != 'publish') {
-    wp_redirect( home_url(), 301 );
+    wp_redirect(home_url(), 301);
     exit;
 }
 
@@ -22,6 +22,7 @@ if ($cat_id) {
     $posts_estadisticas = get_posts($args_form);
 }
 
+//var_dump($posts_estadisticas);exit;
 ?>
 
 <!-- Page Preloder -->
@@ -46,14 +47,24 @@ if ($cat_id) {
     </h1>
     <hr/>
     <div>
-        <?php 
-            if (!empty($pagina->post_content)):
-                echo $pagina->post_content;
+        <?php
+            if (isset($posts_estadisticas[0])):
+                //sacar el primero y actualizar array:
+                $post_current = array_shift($posts_estadisticas);
+            
+                $link = get_field('enlace', $post_current);
+                $url = isset($link['url']) ? $link['url'] : '#';
+                //var_dump($url);
+                ?>
+                <div>
+                    <embed src="https://drive.google.com/viewerng/viewer?embedded=true&url=<?php echo $url; ?>" style="width:100%; height:1000px;">
+                </div>
+                <?php
             endif;
         ?> 
     </div>
 
-    <div>
+    <div class='mt-3 mt-lg-4'>
         <div class="card-header" id="reportesAnteriores">
             <h4 class="mb-0">
                 Reportes anteriores
@@ -67,12 +78,15 @@ if ($cat_id) {
 
                     <?php foreach ($posts_estadisticas as $post): ?> 
                         <?php if ($post->post_status !== 'publish'): continue;
-                        endif; ?>   
+                        endif;
+                        ?>   
                         <?php
                         $link = get_field('enlace', $post);
+                        //var_dump($link);
                         ?>
                         <p>
-                            <a href="<?php echo $link; ?>" target="_blank" class="<?php echo $link ? '' : 'disabled' ?>"><?php echo $post->post_title; ?>
+                            <a href="<?php echo isset($link['url']) ? $link['url'] : '#'; ?>" target="_blank" class="<?php echo isset($link['url']) ? '' : 'disabled' ?>">
+                                <?php echo $post->post_title; ?>
                             </a>
                         </p>
 
@@ -80,7 +94,7 @@ if ($cat_id) {
                 <?php else: ?>
                     <p class="text-muted">No se cargado ningun reporte.</p>
                 <?php endif; ?>
-                    
+
             </div>
         </div>
     </div>
