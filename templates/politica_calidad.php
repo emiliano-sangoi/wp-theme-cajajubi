ccd95dae7061e336798e4cc27fa16ffe48c62c6e
Emiliano Sangoi <esangoi@santafe.gov.ar>
2022-12-21 08:56:02 -0300

    Nuevo formato para la seccion "Politica de calidad"

--- templates/politica_calidad.php
+++ templates/politica_calidad.php
@@ -10,165 +10,154 @@ if (!$pagina instanceof WP_Post || $pagina->post_status != 'publish') {
 get_header();
 ?>
 
-<!-- Page Preloder -->
-<!--<div id="preloder">
-    <div class="loader"></div>
-</div>-->
+    <!-- Page Preloder -->
+    <!--<div id="preloder">
+        <div class="loader"></div>
+    </div>-->
 
-<div class="container navbar-separator px-3 px-lg-5 pt-3 pb-5 altura-minima" id="quienes-somos">
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="<?php echo get_home_url(); ?>">Inicio</a></li>   
-            <li class="breadcrumb-item">Institucional</li> 
-            <li class="breadcrumb-item active" aria-current="page">
-                <?php echo $pagina->post_title; ?>
-            </li>
-        </ol>
-    </nav>
-
-
-    <h1 class="font-weight-bold text-primary">
-        <?php echo $pagina->post_title; ?>
-    </h1>
-    <hr/>
-
-    <div>
-        <?php
-        if (!empty($pagina->post_content)):
-            echo nl2br($pagina->post_content);
-            ?>
-        <?php endif; ?>
-    </div>
+    <div class="container navbar-separator px-3 px-lg-5 pt-3 pb-5 altura-minima" id="quienes-somos">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="<?php echo get_home_url(); ?>">Inicio</a></li>
+                <li class="breadcrumb-item">Institucional</li>
+                <li class="breadcrumb-item active" aria-current="page">
+                    <?php echo $pagina->post_title; ?>
+                </li>
+            </ol>
+        </nav>
 
 
-    <section class="mt-4 mb-3">
-        <div class='card-deck'>
-            <div class='card border mision'>
-                <div class='card-body'>
-                    <h3 class="card-title font-weight-bold text-center">
-                        <img class="w-20 card-img-top align-middle" src="<?php echo DIR_IMGS . '/iconos/dart-board-and-arrow.png' ?>" alt=""/>
-                        Misi&oacute;n
-                    </h3>
-                    <!--<hr class="border-light w-50"/>-->
-                    <p class="card-text py-2 py-lg-3">
-                        Prestar un servicio público de excelencia, desarrollando los procesos de inicio de trámites y de atención al público y asesoramiento de manera óptima y ágil, minimizando los tiempos de espera y de gestión de las actuaciones administrativas, con un alto grado de satisfacción de los ciudadanos, mejorando permanentemente nuestros procesos.
-                    </p>
-                </div>
-            </div>
+        <h1 class="font-weight-bold text-primary">
+            <?php echo $pagina->post_title; ?>
+        </h1>
+        <hr/>
 
-            <div class='card border vision'>
-                <div class='card-body'>
-                    <h3 class="card-title font-weight-bold text-center">
-                        <img class="w-20 card-img-top align-middle" src="<?php echo DIR_IMGS . '/iconos/eye.png' ?>" alt=""/>                        
-                        Visi&oacute;n
-                    </h3>
-                    <!--<hr class="border-light w-50"/>-->
-                    <p class="card-text py-2 py-lg-3">
-                        Ser un organismo ejemplar en el marco de las instituciones de seguridad social administradas por el Estado, con un sistema de gestión moderno y eficiente, que cumpla satisfactoriamente todos los requisitos aplicables y se encuentre fuertemente orientado a la satisfacción total de nuestros afiliados, beneficiarios y el resto de las partes interesadas pertinentes.
-                    </p>
-                </div>
-            </div>
-
-            <div class='card border valores'>
-                <div class='card-body'>
-                    <h3 class="card-title font-weight-bold text-center">
-                        <img class="w-20 card-img-top align-middle" src="<?php echo DIR_IMGS . '/iconos/value.png' ?>" alt=""/>
-                        Valores
-                    </h3>
-                    <!--<hr class="border-light w-50"/>-->
-                    <div class="card-text py-2 py-lg-3">
-                        <p>La organización considera como valores claves:</p>
-                        <ul class="list-unstyled">
-                            <li>
-                                <i class="fa fa-check"></i>&nbsp;
-                                Ética y transparencia de gestión.
-                            </li>
-                            <li>
-                                <i class="fa fa-check "></i>&nbsp;
-                                Eficiencia.
-                            </li>
-                            <li>
-                                <i class="fa fa-check "></i>&nbsp;
-                                Honestidad y Respeto a nivel personal y profesional.
-                            </li>
-                            <li>
-                                <i class="fa fa-check "></i>&nbsp;
-                                Lealtad a la Institución y compromiso con los objetivos de la misma.
-                            </li>
-                            <li>
-                                <i class="fa fa-check "></i>&nbsp;
-                                Innovación permanente.
-                            </li>
-                            <li>
-                                <i class="fa fa-check "></i>&nbsp;
-                                Entusiasmo colectivo.
-                            </li>
-                        </ul> 
-                    </div>
-                </div>
-            </div>
-
+        <div>
+            <?php
+            if (!empty($pagina->post_content)):
+                echo nl2br($pagina->post_content);
+                ?>
+            <?php endif; ?>
         </div>
 
+        <?php
+
+        $mision = get_field('mision', $pagina);
+        $valores = get_field('valores', $pagina);
+
+        ?>
 
 
-    </section>
-
-
-
-    <!--
-        <div class="row mt-4">
-            <div class="col-4">
-                <div class="card border-primary mb-3 w-100">
-                    <div class="card-header">
-                        <h4 class="mb-0 font-weight-bold">Misi&oacute;n</h4>
-                    </div>
-                    <div class="card-body">                    
-                        <p class="card-text">
-                            Prestar un servicio público de excelencia, desarrollando los procesos de inicio de trámites y de atención al público y asesoramiento de manera óptima y ágil, minimizando los tiempos de espera y de gestión de las actuaciones administrativas, con un alto grado de satisfacción de los ciudadanos, mejorando permanentemente nuestros procesos.
-                        </p>
-                    </div>
-                </div>
-            </div>
-    
-            <div class="col-4">
-                <div class="card border-primary mb-3 w-100">
-                    <div class="card-header">
-                        <h4 class="mb-0 font-weight-bold">
+        <section class="mt-4 mb-3">
+            <div class='card-deck'>
+                <div class='card border-danger shadow-sm' style="border-width: 2px;">
+                    <div class='card-body'>
+                        <h3 class="card-title font-weight-bold text-danger text-center">
+<!--                            <i class="fa fa-circle small"></i>&nbsp;-->
                             Visi&oacute;n
-                        </h4>
-                    </div>
-                    <div class="card-body">                    
-                        <p class="card-text">
-                            Ser un organismo ejemplar en el marco de las instituciones de seguridad social administradas por el Estado, con un sistema de gestión moderno y eficiente, que cumpla satisfactoriamente todos los requisitos aplicables y se encuentre fuertemente orientado a la satisfacción total de nuestros afiliados, beneficiarios y el resto de las partes interesadas pertinentes.
-                        </p>
-                    </div>
-                </div>
-            </div>
-    
-            <div class="col-4">
-                <div class="card border-primary mb-3 w-100">
-                    <div class="card-header">
-                        <h4 class="mb-0 font-weight-bold">
-                            Valores
-                        </h4>
-                    </div>
-                    <div class="card-body">                    
-                        <div class="card-text">
-                            <p>La organización considera como valores claves:</p>
-                            <ul>
-                                <li>Ética y transparencia de gestión.</li>
-                                <li>Eficiencia.</li>
-                                <li>Honestidad y Respeto a nivel personal y profesional.</li>
-                                <li>Lealtad a la Institución y compromiso con los objetivos de la misma.</li>
-                                <li>Innovación permanente.</li>
-                                <li>Entusiasmo colectivo.</li>
-                            </ul>                
+                        </h3>
+                        <hr class="border-danger w-75" style="border-width: 2px;" />
+                        <div class="card-text p-2 p-lg-3 text-lead">
+                            <?php
+                            $vision = get_field('vision', $pagina);
+                            echo nl2br($vision);
+                            ?>
                         </div>
                     </div>
                 </div>
+
+                <div class='card border-success shadow-sm' style="border-width: 2px;">
+                    <div class='card-body'>
+                        <h3 class="card-title font-weight-bold text-success text-center">
+<!--                            <i class="fa fa-circle small"></i>&nbsp;-->
+                            Misi&oacute;n
+                        </h3>
+                        <hr class="border-success w-75" style="border-width: 2px;" />
+                        <div class="card-text p-2 p-lg-3">
+                            <?php
+                            $mision = get_field('mision', $pagina);
+                            echo nl2br($mision);
+                            ?>
+                        </div>
+                    </div>
+                </div>
+
+                <div class='card border-info shadow-sm' style="border-width: 2px;">
+                    <div class='card-body'>
+                        <h3 class="card-title font-weight-bold text-info text-center">
+                            <!--                            <i class="fa fa-circle small"></i>&nbsp;-->
+                            Valores
+                        </h3>
+                        <hr class="border-info w-75" style="border-width: 2px;" />
+                        <!--<hr class="border-light w-50"/>-->
+                        <div class="card-text p-2 p-lg-3">
+                            <?php
+                            $valores = get_field('valores', $pagina);
+                            echo nl2br($valores);
+                            ?>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-        </div>-->
-</div>
+
+
+        </section>
+
+
+        <!--
+            <div class="row mt-4">
+                <div class="col-4">
+                    <div class="card border-primary mb-3 w-100">
+                        <div class="card-header">
+                            <h4 class="mb-0 font-weight-bold">Misi&oacute;n</h4>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">
+                                Prestar un servicio público de excelencia, desarrollando los procesos de inicio de trámites y de atención al público y asesoramiento de manera óptima y ágil, minimizando los tiempos de espera y de gestión de las actuaciones administrativas, con un alto grado de satisfacción de los ciudadanos, mejorando permanentemente nuestros procesos.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-4">
+                    <div class="card border-primary mb-3 w-100">
+                        <div class="card-header">
+                            <h4 class="mb-0 font-weight-bold">
+                                Visi&oacute;n
+                            </h4>
+                        </div>
+                        <div class="card-body">
+                            <p class="card-text">
+                                Ser un organismo ejemplar en el marco de las instituciones de seguridad social administradas por el Estado, con un sistema de gestión moderno y eficiente, que cumpla satisfactoriamente todos los requisitos aplicables y se encuentre fuertemente orientado a la satisfacción total de nuestros afiliados, beneficiarios y el resto de las partes interesadas pertinentes.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-4">
+                    <div class="card border-primary mb-3 w-100">
+                        <div class="card-header">
+                            <h4 class="mb-0 font-weight-bold">
+                                Valores
+                            </h4>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-text">
+                                <p>La organización considera como valores claves:</p>
+                                <ul>
+                                    <li>Ética y transparencia de gestión.</li>
+                                    <li>Eficiencia.</li>
+                                    <li>Honestidad y Respeto a nivel personal y profesional.</li>
+                                    <li>Lealtad a la Institución y compromiso con los objetivos de la misma.</li>
+                                    <li>Innovación permanente.</li>
+                                    <li>Entusiasmo colectivo.</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>-->
+    </div>
 
 <?php get_footer(); ?>
